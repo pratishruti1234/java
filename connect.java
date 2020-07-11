355f0062eac1f04b74a9a291b6d76df6774c392d
pratishruti1234 <66667368+pratishruti1234@users.noreply.github.com>
2020-07-11 19:05:59 +0530

    connect

---
+++ connect.java
@@ -0,0 +1,21 @@
+import java.sql.*;
+import oracle.jdbc.driver.OracleDriver;
+public class connect
+     {
+public static void main (String args[])
+    {
+     try{
+  OracleDriver driver=new OracleDriver();
+  DriverManager.registerDriver(driver);
+  Connection cn=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE","SYSTEM","sini1234");
+       if(cn!=null)
+            {
+      System.out.println("connection object is created");
+            }
+  cn.close();
+        }catch(SQLException se)
+             {
+             se.printStackTrace();
+             }
+     }
+  }
